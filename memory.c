60dbaace70cf0ef9a8183181e8bece659c3a1c90
“TarunSinghD” <Tarun.Singh@colorado.edu>
2016-09-16 23:56:20 -0600

    Added my_memzero and my_reverse functions

--- memory.c
+++ memory.c
@@ -1,5 +1,8 @@
 #include "memory.h"
 
+/* A loop variable */
+uint8_t i;
+
 /* Description:
  * This function moves "length" number of bytes of data starting from memory location pointed by src to memory location pointed by dst
  *
@@ -22,9 +25,6 @@ uint8_t my_memmove(uint8_t * src, uint8_t * dst, uint32_t length)
 	/* A temporary array to store n bytes of data from the source location */
 	uint8_t temp[length];
 
-	/* A loop variable */
-	uint8_t i = 0;
-
 	/* Copying the source contents to temporary memory location */
 	for (i = 0; i < length; i++)
 	{
@@ -35,6 +35,51 @@ uint8_t my_memmove(uint8_t * src, uint8_t * dst, uint32_t length)
 	for (i = 0; i < length; i++)
 	{
 		*(dst + i) = *(temp + i);
+	}
+
+	return 0;
+}
+
+/* Description:
+ * This function fills "length" number of bytes of data starting from memory location pointed by src
+ *
+ * Inputs:
+ * a) A pointer to the source memory location, src.
+ * b) The number of bytes to be reset to zero, length.
+ *
+ * Output/ Return value:
+ * a) Zero to indicate successful completion of memmove.
+ * b) Any non zero value to indicate failure.
+ */
+uint8_t my_memzero(uint8_t * src, uint32_t length)
+{
+	/* Reset the memory location 1 byte at a time */
+	for (i = 0; i < length; i++)
+	{
+		*(src + i) = 0;
+	}
+
+	return 0;
+}
+	
+/* Description:
+ * This function fills "length" number of bytes of data starting from memory location pointed by src
+ *
+ * Inputs:
+ * a) A pointer to the source memory location, src.
+ * b) The number of bytes to be considered for reverse, length.
+ *
+ * Output/ Return value:
+ * a) Zero to indicate successful completion of memmove.
+ * b) Any non zero value to indicate failure.
+ */
+uint8_t my_reverse(uint8_t * src, uint32_t length)
+{
+	/* Start swapping the characters, first character with the last character, second character with the second last and so on */
+
+	for (i = 0; i < (length / 2); i++)
+	{
+		*(src + i) = *(src + length - 1 - i);
 	}
 
 	return 0;
--- memory.h
+++ memory.h
@@ -4,5 +4,7 @@
 
 /* Function prototype */
 uint8_t my_memmove(uint8_t * src, uint8_t * dst, uint32_t length);
+uint8_t my_memzero(uint8_t * src, uint32_t length);
+uint8_t my_reverse(uint8_t * src, uint32_t length);
 
 #endif
