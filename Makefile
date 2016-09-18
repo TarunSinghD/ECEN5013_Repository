255e8c73f617f34248a97c473fd4f2a03d198207
“TarunSinghD” <Tarun.Singh@colorado.edu>
2016-09-18 11:51:01 -0600

    project_1.c and project_1.h added and tested. Working finegit add -A

---
+++ Makefile
@@ -0,0 +1,2 @@
+main : project_1.c main.c memory.c
+	gcc project_1.c main.c memory.c -o main -Wall
---
+++ main.c
@@ -0,0 +1,9 @@
+#include "project_1.h"
+#include "memory.h"
+
+int main()
+{
+	project_1_report();
+
+	return 0;
+}
