c2838c753e4ef9aa5abdeb94096014163c68353d
“TarunSinghD” <Tarun.Singh@colorado.edu>
2016-11-09 12:10:24 -0700

    Added user friendly macros to control the LED in led.h

--- led.h
+++ led.h
@@ -9,6 +9,10 @@
 #define INCLUDES_LED_H_
 
 #include "MKL25Z4.h"
+#define RED(x)		TPM2_BASE_PTR->CONTROLS[0].CnV = TPM2_BASE_PTR->MOD * x
+#define GREEN(x)	TPM2_BASE_PTR->CONTROLS[1].CnV = TPM2_BASE_PTR->MOD * x
+#define BLUE(x)		TPM0_BASE_PTR->CONTROLS[1].CnV = TPM0_BASE_PTR->MOD * x
+
 void led_init(void);
 
 uint8_t prev_led;
